1dcbd76042baf46ecb9a686c3b66ad67c4604363
Yunqiu Han <yunqiu21@g.ucla.edu>
2023-04-16 02:54:47 +0000

    * Parse Content-Length from header and only read   content_length bytes from body * Include Content-Length field in response header * Reports error when the input is not an HTTP request
    Change-Id: I380d4b30f40211bdff5d577a5d07c6fb1a42f71e

--- src/session.cc
+++ src/session.cc
@@ -4,7 +4,8 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/bind.hpp>
-
+#include <boost/beast/http.hpp>
+#include <boost/optional/optional_io.hpp>
 #include "session.h"
 
 tcp::socket& session::socket() {
@@ -22,7 +23,6 @@ void session::handle_read(const boost::system::error_code& error,
                           size_t bytes_transferred) {
   if (!error) {
     // parse header
-    // TODO(yunqiu21): parse the Content-Length field
     std::string data_str(data_);
     int pos;
     if ((pos = data_str.find("\r\n\r\n")) == std::string::npos) {
@@ -31,13 +31,33 @@ void session::handle_read(const boost::system::error_code& error,
       return;
     }
     pos += 4;
+    // parse the Content-Length field
+    boost::beast::http::request_parser<boost::beast::http::string_body> parser;
+    boost::beast::error_code ec;
+    std::size_t bytes_parsed = parser.put(boost::asio::buffer(data_), ec);
+    if (ec) {
+      std::cerr << "Error parsing HTTP header: " << ec.message() << std::endl;
+      delete this;
+      return;
+    }
+    boost::optional<std::uint64_t> content_len = parser.content_length();
+    if (!content_len.has_value()) {
+      std::cerr << "Missing content length field" << std::endl;
+      delete this;
+      return;
+    }
     // compose response
-    // TODO(yunqiu21): add a Content-Length field
-    std::string response =
-        "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n" +
-        std::string(data_ + pos, bytes_transferred - pos);
+    int body_len = *content_len > bytes_transferred - pos ? bytes_transferred - pos : *content_len;
+    std::string response_body = std::string(data_ + pos, body_len);
+    std::ostringstream response_oss;
+    response_oss << "HTTP/1.1 200 OK\r\n";
+    response_oss << "Content-Type: text/plain\r\n";
+    response_oss << "Content-Length: " << response_body.length() << "\r\n";
+    response_oss << "\r\n";
+    response_oss << response_body;
+    std::string response_buf = response_oss.str();
     // write to socket
-    boost::asio::async_write(socket_, boost::asio::buffer(response),
+    boost::asio::async_write(socket_, boost::asio::buffer(response_buf),
                              boost::bind(&session::handle_write, this,
                              boost::asio::placeholders::error));
   } else {
