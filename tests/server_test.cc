94a09f37337f8efc997373333fc9bc6351486758
Yunqiu Han <yunqiu21@g.ucla.edu>
2023-04-22 07:44:08 +0000

    Add test for server Change cmakelist accordingly
    Change-Id: I8ead1c90d2262563b1f2dd39ee1a0433f723c29f

---
+++ tests/server_test.cc
@@ -0,0 +1,27 @@
+#include "config_manager.h"
+#include "server.h"
+#include "session.h"
+#include "gtest/gtest.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class serverTest : public ::testing::Test {
+protected:
+    serverTest() {}
+    ~serverTest() {
+        delete config_manager;
+        delete test_server;
+    }
+    boost::asio::io_service io_service;
+    ConfigManager *config_manager;
+    server *test_server;
+};
+
+TEST_F(serverTest, ConstructServer) {
+    config_manager = ConfigManager::makeConfigManager("server_test_config");
+    test_server = new server(io_service, config_manager);
+    using namespace std::chrono_literals;
+    io_service.run_for(1s);
+    exit(0);
+}
