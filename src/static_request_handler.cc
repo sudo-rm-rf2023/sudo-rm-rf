9db9b1f0d2179b2249ee12391b8437d839f6f864
Qianli <qianliwu@g.ucla.edu>
2023-05-03 06:46:51 +0000

    fix bug: jpeg in mime_type
    Change-Id: I431e5c3336e75d7d43b090ea9426e6c51f633393

--- src/static_request_handler.cc
+++ src/static_request_handler.cc
@@ -27,6 +27,7 @@ std::string StaticRequestHandler::mime_type(const std::string& path) {
         {".json", "application/json"},
         {".png", "image/png"},
         {".jpg", "image/jpeg"},
+        {".jpeg", "image/jpeg"},
         {".gif", "image/gif"},
         {".svg", "image/svg+xml"},
         {".ico", "image/x-icon"},
