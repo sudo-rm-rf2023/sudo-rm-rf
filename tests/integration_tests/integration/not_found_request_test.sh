d9fa60007d7876e891a905024e4ddb21bd654585
Yunqiu Han <yunqiu21@g.ucla.edu>
2023-05-17 04:59:21 +0000

    Add test for a request to an unconfigured path, check if it returns "Not Found".
    Change-Id: Ia154a7351fd680db95bbb1a63ef1f793c666ad0f

---
+++ tests/integration_tests/integration/not_found_request_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SCRIPT_DIR="$1"
+TEST_PORT=$2
+EXPECTED_RESPONSE_FILE="expected_404_response.txt"
+ACTUAL_RESPONSE_FILE="actual_404_response.txt"
+
+# Send a request to the web server and capture the output
+curl -D - -X POST -H "Content-Type: text/plain" -d "This is a test message" -s -S http://localhost:${TEST_PORT}/notfoundpath > "${SCRIPT_DIR}/$ACTUAL_RESPONSE_FILE"
+
+# Verify that the response from the web server is as expected
+# -b option ignores line endings (Windows vs. Linux)
+diff -b "${SCRIPT_DIR}/$EXPECTED_RESPONSE_FILE" "${SCRIPT_DIR}/$ACTUAL_RESPONSE_FILE"
+DIFF_EXIT_CODE=$?
+
+# Continue to next test or exit with failure (1)
+if [ $DIFF_EXIT_CODE -eq 0 ]; then
+  echo "PASS"
+else
+  echo "404 not found test FAIL"
+fi
