ff60a47e2abdc8dbc19750c06ab2dfc6e152ca90
Devel User <milok23@g.ucla.edu>
2023-05-20 23:49:52 +0000

    Adding CRUD API Functions to the CRUD_API_handler header.
    Add the functions to the header file only -- no implementation.
    
    No tests affected, all tests still pass.
    
    Why: Allows easy implementation of CRUDL functions in the handler.
    
    TODO: Implement the functions (milok23@ thuang45@)
    
    Change-Id: I8504459e8f441e630b8680305cf2bf604d695c6f

--- include/crud_api_handler.h
+++ include/crud_api_handler.h
@@ -14,6 +14,45 @@ class CRUDApiHandler : public RequestHandler {
 
   status handle_request(const http::request<http::string_body> &request,
                         http::response<http::string_body> &response) override;
+
+ private:
+  /*
+   * API: Creates an entity and responds with the new ID.
+   * Internally: Finds next available ID and writes req body to
+   * {data_path}/{entity}/{ID}. Returns {"id": {ID}} in the response.
+   */
+  status handle_create_request(const http::request<http::string_body> &request,
+                               http::response<http::string_body> &response);
+
+  /*
+   * API: Returns the data for previously created entity + ID.
+   * Internally: Reads the stored {data_path}/{entity}/{ID} and resp to user.
+   */
+  status handle_retrieve_request(
+      const http::request<http::string_body> &request,
+      http::response<http::string_body> &response);
+
+  /*
+   * API: Updates the data for entity/ID with new data, resp w/ status.
+   * Internally: writes req body to
+   * {data_path}/{entity}/{ID}, responds "SUCCESS" or "FAIL" if DNE.
+   */
+  status handle_update_request(const http::request<http::string_body> &request,
+                               http::response<http::string_body> &response);
+  /*
+   * API: Delete entity w/ ID from the system
+   * Internally: Removes file {data_path}/{entity}/{ID} and respond "SUCCESS" if
+   * it exists, else response "FAIL"
+   */
+  status handle_delete_request(const http::request<http::string_body> &request,
+                               http::response<http::string_body> &response);
+  /*
+   * API: Returns a JSON list of valid IDs for the given Entity.
+   * Internally: Lists filenames in {data_path}/{entity}. Returns the list of file
+   * names in the response, or empty list for no existing entity.
+   */
+  status handle_list_request(const http::request<http::string_body> &request,
+                             http::response<http::string_body> &response);
 };
 
 class CRUDApiHandlerFactory : public RequestHandlerFactory {
